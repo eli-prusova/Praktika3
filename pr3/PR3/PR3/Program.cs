b5d2b0264be5d585ff79b7dd92929d28690460f2
eli-prusova <prussia01@yandex.ru>
2020-09-18 11:22:53 +0300

    fhkbyh

--- pr3/PR3/PR3/Program.cs
+++ pr3/PR3/PR3/Program.cs
@@ -1,35 +1,18 @@
 ﻿using System;
-using System.Text.RegularExpressions;
+using System.IO;
+using System.Text;
 
-namespace ConsoleApp1
+namespace pr3_01_upr2
 {
   class Program
   {
-    static void Main(string[] args) 
+    static void Main(string[] args)
     {
-      string[] text ={ 
-        "University ", 
-        "C++", 
-        " +79263772622",
-        " (555)555-12122",
-        " (926)3772622" };
-
-      foreach (string item in text)
-      {
-        string pattern = @"\d{7}";
-        if (Regex.IsMatch(item, pattern))
-        {
-          //Console.WriteLine($"{item} это телефон"); 
-          Console.WriteLine(item + " это телефон");
-        }
-        else
-        {
-          Console.WriteLine(
-          string.Format("{0} это НЕ телефон", item)
-          );
-        }
-
-      }
+      StreamReader sr = new StreamReader(@"boot.ini");
+      StreamWriter sw = new StreamWriter(@"boot - utf7.txt", false, Encoding.UTF7);
+      sw.WriteLine(sr.ReadToEnd());
+      sw.Close();
+      sr.Close();
 
     }
   }
