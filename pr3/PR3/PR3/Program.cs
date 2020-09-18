1cd8f7760a6860934bbb58f38251e9675aea6f29
eli-prusova <prussia01@yandex.ru>
2020-09-18 10:57:22 +0300

    wert

---
+++ pr3/PR3/PR3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    static void Main(string[] args) 
+    {
+      string[] text ={ 
+        "University", 
+        "C++", 
+        " +79263772622",
+        " (555)555-12122",
+        " (926)3772622" };
+
+      foreach (string item in text)
+      {
+        string pattern = @"\d{7}";
+        if (Regex.IsMatch(item, pattern))
+        {
+          //Console.WriteLine($"{item} это телефон"); 
+          Console.WriteLine(item + " это телефон");
+        }
+        else
+        {
+          Console.WriteLine(
+          string.Format("{0} это НЕ телефон", item)
+          );
+        }
+
+      }
+
+    }
+  }
+}
